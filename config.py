0acb8ecaf929af1c5fd794f2e51ee38642f1ff0f
Ian <ikennedy@natrent0.csde.washington.edu>
2020-04-02 15:19:48 -0700

    fixed setup for production server

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
+from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+load_dotenv(os.path.join(basedir, '.env'))
 
 class BaseConfig:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ['SECRET_KEY'] or 'you-will-never-guess'
     LOCATION = 'LOCATION'
