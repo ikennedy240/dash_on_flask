152f8045b87df1358f70607e9d27848d0067e3dd
Ian <ikennedy@natrent0.csde.washington.edu>
2020-04-06 17:01:29 -0700

    added select circles

--- app/dashapp1/layout.py
+++ app/dashapp1/layout.py
@@ -1,19 +1,18 @@
 import dash_core_components as dcc
 import dash_html_components as html
 import sqlite3
+from app.dashapp1.callbacks import psql_connect
 
 
 
-
-conn = sqlite3.connect('data/clData.db')
+conn = psql_connect()
 c = conn.cursor()
-c.execute('SELECT listing_loc, COUNT(*) as count FROM listings GROUP BY listing_loc ORDER BY count DESC LIMIT 10')
+c.execute('SELECT DISTINCT listing_loc FROM clean')
 cities = c.fetchall()
 cities = [{'label': city[0], 'value': city[0]} for city in [('Average Across Locations',0)]+cities]
 
 layout = html.Div([
     html.H3('Scraper Status Dashboard'),
-    dcc.Graph(id='scrapers_map'),
     html.H1('Scraped Data By Location and Date'),
     dcc.Dropdown(
         id='my-dropdown',
@@ -21,7 +20,9 @@ layout = html.Div([
         value='Average Across Locations',
         multi=True
     ),
-    dcc.Graph(id='my-graph')
+    dcc.Graph(id='scrapers-map'),
+    dcc.Graph(id='listing-graph'),
+    dcc.Graph(id='log-graph')
 ], style={'width': '500'})
 
 if __name__ == '__main__':
